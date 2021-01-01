1a185a7288bf9441e370e6995f6a15ac513c5003
AndrosVAV <androsveres@gmail.com>
2021-01-01 22:47:59 +0400

    Управление мячом с клавиатуры

--- script.js/jsFor Children.js
+++ script.js/jsFor Children.js
@@ -680,8 +680,119 @@ setInterval(function(){
 
     ctx.strokeRect(0,0,200,200);//видимые границы
 },30);
-
 */
+
+
+
+//Управление анимациями с клавиатуры(c.224);
+//Обработчик события keydown(c.215);
+
+/*
+var keyNames = {
+    32 : "spase",
+    37 : "left",
+    38 : "up",
+    39 : "right",
+    40 : "down"
+    };
+    
+    $("body").keydown(function(event){
+    console.log(event.keyCode);
+    });
+    */
+
+    //Управление мячом с клавиатуры(c.227);
+
+
+    
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var width = canvas.width;
+var height = canvas.height;
+
+var circle = function(x,y,radius,fillCircle){
+	ctx.beginPath();
+	ctx.arc(x,y,radius,0,Math.PI * 2,false);
+	if(fillCircle){
+	ctx.fill();
+	}else{
+	ctx.stroke();
+	}
+};
+var Ball = function(){
+	this.x = width / 2;
+	this.y = height /2;
+	this.xSpeed = 5;
+	this.xSpeed = 0;
+};
+
+Ball.prototype.move = function(){
+	this.x += this.xSpeed;
+	this.y += this.ySpeed;
+	if(this.x < 0){
+	this.x = width;
+	}else if(this.x > width){
+	this.x = 0;
+	}else if(this.y < 0){
+	this.y = height;
+	}else if(this.y > height){
+	this.y = 0;
+	}
+};
+
+Ball.prototype.draw = function(){
+	circle(this.x,this.y,10,true);
+};
+
+Ball.prototype.setDirection = function(direction){
+	if(direction === "up"){
+	this.xSpeed = 0;
+	this.ySpeed = -5;
+	}else if(direction === "down"){
+	this.xSpeed = 0;
+	this.ySpeed = 5;
+	}else if(direction === "left"){
+	this.xSpeed = -5;
+	this.ySpeed = 0;
+	}else if(direction === "right"){
+	this.xSpeed = 5;
+	this.ySpeed = 0;
+	}else if(direction === "stop"){
+	this.xSpeed = 0;
+	this.ySpeed = 0;
+	}
+};
+
+var ball = new Ball();
+
+var keyActions = {
+    32 : "stop",
+    37 : "left",
+    38 : "up",
+    39 : "right",
+    40 : "down"
+    };
+    
+    $("body").keydown(function(event){
+	var direction = keyActions[event.keyCode];
+    ball.setDirection(direction);
+    });
+	
+setInterval(function(){
+	ctx.clearRect(0,0,width,height);
+	ball.draw();
+	ball.move();
+	ctx.strokeRect(0,0,width,height);
+},30);	
+
+
+
+
+    
+
+
+
+
 
 
 
