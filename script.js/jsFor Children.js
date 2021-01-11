7f42a47893c1f21fb2a544053c5f82ed55998ee1
AndrosVAV <androsveres@gmail.com>
2021-01-11 12:59:12 +0400

    Игра змейка2

--- script.js/jsFor Children.js
+++ script.js/jsFor Children.js
@@ -1042,6 +1042,140 @@ var gameOver = function(){
 
 
 
+//Игра змейка часть2(c.252);
+
+var Block = function(col,row){
+	this.col = col;//столбец и строка
+	this.row = row;
+};
+//создание обьекта для ячейки
+//var sampleBlock = new Block(5,5);
+//Метод drawSquare(c.253);
+Block.prototype.drawSquare = function(color){
+	var x = this.col * blockSize;
+	var y = this.row * blockSize;
+	ctx.fillStyle = color;
+	ctx.fillRect(x,y,blockSize,blockSize);
+};
+/*
+//Проверка
+var sampleBlock = new Block(3,4);
+sampleBlock.drawSquare("LightBlue");
+*/
+//Функция circle из главы(c.212);
+var circle = function(x,y,radius,fillCircle){
+	ctx.beginPath();
+	ctx.arc(x,y,radius,0,Math.PI * 2,false);
+	if(fillCircle){
+	ctx.fill();
+	}else{
+	ctx.stroke();
+	}
+}; 
+
+//Добавляем метод drawCircle(c.255);
+Block.prototype.drawCircle = function(color){
+		var centerX = this.col * blockSize + blockSize / 2;
+		var centerY = this.row * blockSize + blockSize / 2;
+		ctx.fillStyle = color;
+		circle(centerX,centerY,blockSize / 2,true);
+};
+
+/*
+// Проверка
+var sampleCircle = new Block(4,3);
+sampleCircle.drawCircle("LightGreen");
+*/
+
+//Метод equal(c.256);
+
+Block.prototype.equal = function(otherBlock){
+	return this.col === otherBlock && this.row === otherBlock;
+};
+
+
+					/*
+					var apple = new Block(2,5);
+					var head = new Block(3,5);
+					console.log(head.equal(apple));
+					console.log(apple.equal(head));
+
+
+					var apple = new Block(3,3);
+					var head = new Block(3,3);
+					console.log(head.equal(apple));
+					console.log(apple.equal(head));
+					*/
+
+//Создаем змейку(c.257);
+//Пишем конструктор Snake;
+
+var Snake = function(){
+	this.segments = [
+	new Block(7,5),
+	new Block(6,5),
+	new Block(5,5)
+	];
+	this.direction = "right";
+	this.nextDirection = "right";
+};
+//Рисуем змейку(c.258);
+
+Snake.prototype.draw = function(){
+for(var i = 0;i < this.segments.length;i++){
+	this.segments[i].drawSquare("Blue");
+}
+};
+
+/*
+//Проверка
+var snake = new Snake();
+snake.draw();
+*/
+
+//Перемещаем змейку(c.259);
+//Добавляем метод move;
+
+Snake.prototype.move = function(){
+	var head = this.segments[0];
+	var newHead;
+	
+	this.direction = this.nextDirection;
+	if(this.direction === "right"){
+		newHead = new Block(head.col + 1,head.row);
+		}
+		else if(this.direction === "down"){
+		newHead = new Block(head.col,head.row + 1);
+		}
+		else if(this.direction === "left"){
+		newHead = new Block(head.col - 1,head.row);
+		}
+		else if(this.direction === "up"){
+		newHead = new Block(head.col,head.row - 1);
+		}
+		
+		if(this.checkCollision(newHead)){
+			gameOver();
+			return;
+		}
+		this.segments.unshift(newHead);
+		
+		if(newHead.equal(apple.position)){
+		score++;
+		apple.move();
+		}else{
+		this.segments.pop();
+		}
+};
+
+
+
+
+
+
+
+
+
 
 
 
